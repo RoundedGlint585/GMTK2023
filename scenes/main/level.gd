fe56eab14d96e5ff484ae858c3a2a5f34560060b
karasikrus <karasikrus@gmail.com>
2023-07-09 19:04:06 +0300

    level 5

--- scenes/main/level.gd
+++ scenes/main/level.gd
@@ -19,4 +19,5 @@ func change_level():
 	pass
 # Called every frame. 'delta' wis the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("skip"):
+		change_level()
